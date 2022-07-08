35e173c44518073137742dca7fa10b1acef486b9
Lewis W <lewis@wilsonleo.com>
2022-07-08 18:55:11 +0100

    Stub out endpoints with OpenAPI definitions

--- LeedsBeerQuest/VenuesApi.cs
+++ LeedsBeerQuest/VenuesApi.cs
@@ -37,6 +37,47 @@ namespace LeedsBeerQuest
 
             return new OkObjectResult(responseMessage);
         }
+
+        [FunctionName("GetVenues")]
+        [OpenApiOperation(operationId: "GetVenues", tags: new[] { "name" })]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> GetVenues(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Venues")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+           
+            return new OkResult();
+        }
+
+        [FunctionName("GetVenuesWithinDistance")]
+        [OpenApiOperation(operationId: "GetVenuesWithinDistance", tags: new[] { "name" })]
+        [OpenApiParameter(name: "distance", In = ParameterLocation.Query, Required = true, Type = typeof(int), Description = "The distance in meters to search for venues")]
+        [OpenApiParameter(name: "position", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The position to search from (lat,long)")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> GetVenuesWithinDistance(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetVenuesWithinDistance")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+           
+            return new OkResult();
+        }
+
+        [FunctionName("GetVenuesWithTag")]
+        [OpenApiOperation(operationId: "GetVenuesWithTag", tags: new[] { "name" })]
+        [OpenApiParameter(name: "tag", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "The tag to search for")]
+        [OpenApiParameter(name: "distance", In = ParameterLocation.Query, Required = false, Type = typeof(int), Description = "The distance in meters to search for venues")]
+        [OpenApiParameter(name: "position", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "The position to search from (lat,long)")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> GetVenuesWithTag(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetVenuesWithTag")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+           
+            return new OkResult();
+        }
     }
 }
 
